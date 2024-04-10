e822fb09ae70362421118f23a083e7cba1f30645
bane-the-dev <159306929+bane-the-dev@users.noreply.github.com>
2024-04-10 11:58:25 +0100

    added the cancelOrder event to the exchange

--- src/interfaces/IExchange.sol
+++ src/interfaces/IExchange.sol
@@ -7,6 +7,7 @@ interface IExchange {
     /* Events */
     event Buy(address indexed buyer, OrderLib.Order sell, bytes32 sellOrderHash);
     event Sell(address indexed seller, OrderLib.Order buyOrder, uint256 tokenId, bytes32 buyOrderHash);
+    event CancelOrder(bytes32 orderHash);
     event NewWhitelistedPaymentToken(address paymentToken);
     event UnWhitelistedPaymentToken(address paymentToken);
     event NewWhitelistedCollection(address collection);
