698f90957b8eeac67e45e1de147dad586d7dd18a
hugoroussel <hroussel96@gmail.com>
2024-04-10 19:08:10 +0100

    NIT

--- test/minter/minterSaveFunds.sol
+++ test/minter/minterSaveFunds.sol
@@ -3,19 +3,6 @@ pragma solidity ^0.8.20;
 import "../base/BaseTest.t.sol";
 
 contract Mint is BaseTest {
-    struct MintConfig {
-        address collection; // The collection address of the NFT
-        uint256 cardsPerPack; // Number of cards per pack
-        uint256 maxPacks; // Total number of packs available for minting
-        address paymentToken; // Token used for payments (address(0) for ETH)
-        uint256 fixedPrice; // Price per pack
-        uint256 maxPacksPerAddress; // max number of packs that can be minted by the same address
-        bool requiresWhitelist; // If true, requires user to be whitelisted
-        bytes32 merkleRoot; // Root of Merkle tree for whitelist verification
-        uint256 startTimestamp; // Start timestamp for minting
-        uint256 expirationTimestamp; // Expiration timestamp for minting
-    }
-
     function setUp() public override {
         super.setUp();
     }
