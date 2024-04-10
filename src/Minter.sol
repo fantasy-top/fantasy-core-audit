2e03eafd45b48896f86c0ffd18d4608e2c671bc9
bane-the-dev <159306929+bane-the-dev@users.noreply.github.com>
2024-04-10 22:50:25 +0100

    removed console

--- src/Minter.sol
+++ src/Minter.sol
@@ -20,8 +20,6 @@ import "./interfaces/IBlast.sol";
 import "./interfaces/IExecutionDelegate.sol";
 import "./interfaces/IFantasyCards.sol";
 import "./interfaces/IMinter.sol";
-// add console
-import "../lib/forge-std/src/console.sol";
 import {wadLn, toDaysWadUnsafe} from "solmate/utils/SignedWadMath.sol";
 
 /// @title A contract for minting Fantasy Cards NFTs using VRGDA pricing
