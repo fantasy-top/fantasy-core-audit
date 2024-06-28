a6001358324a71391068688a60038be0e1b346d8
hugoroussel <hroussel96@gmail.com>
2024-06-28 17:12:04 +0100

    Cleanup code

--- src/interfaces/IFantasyCardsProxy.sol
+++ src/interfaces/IFantasyCardsProxy.sol
@@ -6,28 +6,16 @@ import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 import "@openzeppelin/contracts/interfaces/draft-IERC6093.sol";
 
 interface IFantasyCardsProxy is IERC721Errors {
+
     event ApproveIntegrator(address _contract);
     event DenyIntegrator(address _contract);
-
-    /**
-     * @dev Emitted when `tokenId` token is transferred from `from` to `to`.
-     */
     event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
-
-    /**
-     * @dev Emitted when `owner` enables `approved` to manage the `tokenId` token.
-     */
     event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
-
-    /**
-     * @dev Emitted when `owner` enables or disables (`approved`) `operator` to manage all of its assets.
-     */
     event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
-
     event NewExecutionDelegate(address _executionDelegate);
     event NewFantasyCardsCollection(address _fantasyCards);
 
-    // Functions to approve an integration contract to call the functions
+    // Admin functions to approve an integration contract to call the functions
 
     function approveIntegrator(address _contract) external;
 
